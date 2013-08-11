a13dd8a12b77abc9173916961ccadfd3b2b9a9f7
Ray Chen <clraychen@gmail.com>
2013-08-11 16:25:25 +0800

    start to move the test code to test source folder

---
+++ test/util/TestAnalyze.java
@@ -0,0 +1,57 @@
+package util;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import util.Analyze;
+import data.Component;
+import data.Defect;
+
+public class TestAnalyze {
+
+    private final String RAW_DATA_File_NAME = "testResource/raw_data.txt";
+    private final String RECORDED_EXCEL_FILE = "testResource/CMVC_Trends.xlsx";
+
+    @Test
+    public void testGetNewDefect() {
+        Analyze a = new Analyze();
+        ArrayList<Defect> jitNewResult = a.getNewDefect(Component.JIT,
+                RAW_DATA_File_NAME, RECORDED_EXCEL_FILE);
+        ArrayList<Defect> jitClosedResult = a.getClosedDefect(Component.JIT,
+                RAW_DATA_File_NAME, RECORDED_EXCEL_FILE);
+        int jitNew = jitNewResult.size();
+        int jitClosed = jitClosedResult.size();
+
+        assertEquals(6, jitNew);
+        assertEquals(1, jitClosed);
+
+        Defect closed = jitClosedResult.get(0);
+        assertEquals("197745", closed.getId());
+
+        ArrayList<String> expectedNewDefectsIDs = new ArrayList<String>();
+        expectedNewDefectsIDs.add("198179");
+        expectedNewDefectsIDs.add("198349");
+        expectedNewDefectsIDs.add("198616");
+        expectedNewDefectsIDs.add("198661");
+        expectedNewDefectsIDs.add("198716");
+        expectedNewDefectsIDs.add("198795");
+        // {"196650", "197757", "198179", "198349",
+        // "198616","198661","198716","198795"};
+        ArrayList<String> newDefectsIDs = new ArrayList<String>();
+        for (Defect d : jitNewResult) {
+            newDefectsIDs.add(d.getId());
+        }
+
+        for (String defectID : expectedNewDefectsIDs) {
+            assertTrue(newDefectsIDs.contains(defectID));
+        }
+
+        for (String defectID : newDefectsIDs) {
+            assertTrue(expectedNewDefectsIDs.contains(defectID));
+        }
+    }
+
+}
---
+++ test/util/TestUtils.java
@@ -0,0 +1,146 @@
+package util;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.junit.Test;
+
+import util.Analyze;
+import util.Utils;
+import data.Component;
+import data.Defect;
+
+
+public class TestUtils extends TestCase{
+    
+    private final String RAW_DATA_File_NAME = "testResource/raw_data.txt";
+    private final String RECORDED_EXCEL_FILE = "testResource/CMVC_Trends.xlsx";
+    private final String COPY_OF_RECORDED_EXCEL_FILE = "testResource/Copy_CMVC_Trends.xlsx";
+    
+    private final int vmDefectCount = 71;
+    private final int jclDefectCount = 13;
+    private final int jitDefectCount = 8;
+    
+    private final int vmRecordedDefectCount = 46;
+    private final int jclRecordedDefectCount = 11;
+    private final int jitRecordedDefectCount = 3;
+    
+    @Override
+    public void setUp() throws IOException{
+        copyExcelFile();
+    }
+    
+    @Override
+    public void tearDown(){
+        removeCopyExcelFile();
+    }
+    
+    private void copyExcelFile(){
+        File f = new File(COPY_OF_RECORDED_EXCEL_FILE);
+        if(!f.exists()){
+            try {
+                Utils.copyFile(RECORDED_EXCEL_FILE, COPY_OF_RECORDED_EXCEL_FILE);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    private void removeCopyExcelFile(){
+        File f = new File(COPY_OF_RECORDED_EXCEL_FILE);
+        if(f.exists()){
+            f.delete();
+        }
+    }
+    
+
+    @Test
+    public void testWriteVMNewDefectData() throws InvalidFormatException, IOException{
+        removeCopyExcelFile();
+        copyExcelFile();
+        ArrayList<Defect> existList = Utils.loadRecordedVMData(COPY_OF_RECORDED_EXCEL_FILE);
+        int numOfExistDefect = existList.size();
+        
+        Analyze a = new Analyze();
+        ArrayList<Defect> newList = a.getNewDefect(Component.VM, RAW_DATA_File_NAME, COPY_OF_RECORDED_EXCEL_FILE);
+        Utils.writeVMNewDefectData(COPY_OF_RECORDED_EXCEL_FILE, newList);
+        
+        ArrayList<Defect> addedList = Utils.loadRecordedVMData(COPY_OF_RECORDED_EXCEL_FILE);
+        assertEquals(addedList.size(), numOfExistDefect + newList.size());
+        
+        removeCopyExcelFile();
+    }
+    
+    @Test
+    public void testWriteJCLNewDefectData() throws InvalidFormatException, IOException{
+        removeCopyExcelFile();
+        copyExcelFile();
+        ArrayList<Defect> existList = Utils.loadRecordedJCLData(COPY_OF_RECORDED_EXCEL_FILE);
+        int numOfExistDefect = existList.size();
+        
+        Analyze a = new Analyze();
+        ArrayList<Defect> newList = a.getNewDefect(Component.JCL, RAW_DATA_File_NAME, COPY_OF_RECORDED_EXCEL_FILE);
+        Utils.writeJCLNewDefectData(COPY_OF_RECORDED_EXCEL_FILE, newList);
+        
+        ArrayList<Defect> addedList = Utils.loadRecordedJCLData(COPY_OF_RECORDED_EXCEL_FILE);
+        assertEquals(addedList.size(), numOfExistDefect + newList.size());
+        
+        removeCopyExcelFile();
+    }
+    
+    @Test
+    public void testWriteJITNewDefectData() throws InvalidFormatException, IOException{
+        removeCopyExcelFile();
+        copyExcelFile();
+        ArrayList<Defect> existList = Utils.loadRecordedJITData(COPY_OF_RECORDED_EXCEL_FILE);
+        int numOfExistDefect = existList.size();
+        
+        Analyze a = new Analyze();
+        ArrayList<Defect> newList = a.getNewDefect(Component.JIT, RAW_DATA_File_NAME, COPY_OF_RECORDED_EXCEL_FILE);
+        Utils.writeJITNewDefectData(COPY_OF_RECORDED_EXCEL_FILE, newList);
+        
+        ArrayList<Defect> addedList = Utils.loadRecordedJITData(COPY_OF_RECORDED_EXCEL_FILE);
+        assertEquals(addedList.size(), numOfExistDefect + newList.size());
+        
+        removeCopyExcelFile();
+    }
+
+    @Test
+    public void testVmDefectList() throws IOException{
+        ArrayList<Defect> defectList = Utils.vmDefectList(RAW_DATA_File_NAME);
+        assertEquals(vmDefectCount,defectList.size());
+    }
+    
+    @Test
+    public void testJCLDefectList() throws IOException {
+        ArrayList<Defect> defectList = Utils.jclDefectList(RAW_DATA_File_NAME);
+        assertEquals(jclDefectCount,defectList.size());
+    }
+
+    @Test
+    public void testJITDefectList() throws IOException {
+        ArrayList<Defect> defectList = Utils.jitDefectList(RAW_DATA_File_NAME);
+        assertEquals(jitDefectCount,defectList.size());
+    }
+    
+    @Test
+    public void testLoadRecordedVMData() throws InvalidFormatException, IOException{
+        ArrayList<Defect> defectList = Utils.loadRecordedVMData(RECORDED_EXCEL_FILE);
+        assertEquals(vmRecordedDefectCount, defectList.size());
+    }
+    
+    @Test
+    public void testLoadRecordedJCLData() throws InvalidFormatException, IOException{
+        ArrayList<Defect> defectList = Utils.loadRecordedJCLData(RECORDED_EXCEL_FILE);
+        assertEquals(jclRecordedDefectCount, defectList.size());
+    }
+    
+    @Test
+    public void testLoadRecordedJITData() throws InvalidFormatException, IOException{
+        ArrayList<Defect> defectList = Utils.loadRecordedJITData(RECORDED_EXCEL_FILE);
+        assertEquals(jitRecordedDefectCount, defectList.size());
+    }
+}
